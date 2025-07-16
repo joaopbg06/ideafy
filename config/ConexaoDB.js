4a1df817d9056688ad31e7bfb514ab43a5646d42
GustavoLenni <gustavogithub1@gmail.com>
2025-07-15 22:57:26 -0300

    Configurando Conexao com Sequelize

--- config/ConexaoDB.js
+++ config/ConexaoDB.js
@@ -1,20 +1,23 @@
-// faz a conexao do banco de dados 
-// importando mysql
-const mysql = require("mysql");
+// importando sequelize pegando so a propriedade exclusiva dele usando {}as chaves
+const {Sequelize} = require('sequelize');
 require('dotenv').config();
-// banco de dados
-const conexao = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
 
-if (!conexao){
-    console.log("erro ao conectar");
-}else{
-    console.log("conexao banco funfando");
-}
+// banco de dados
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE, // nome do banco
+    process.env.DB_USER, // usuario
+    process.env.DB_PASSWORD, // senha 
+    {
+        host: process.env.DB_HOST,// host 
+        dialect: 'mysql', // define que é mysql
+        logging:false  // desativa logs SQL (meio que para nao mostrar as queries no console)
+    }
+)
+
+// testar conexao
+sequelize.authenticate()
+    .then(() => console.log("Conexão com o banco funfando!!"))
+    .catch((error) => console.log("Erro ao conectar com o banco!!", error));
 
 // exportando modulo
-module.exports = conexao;
+module.exports = sequelize;
